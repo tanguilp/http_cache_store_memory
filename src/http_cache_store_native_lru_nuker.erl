fc0f264fc0c4c2040ab657ef86599d38e1cfec11
Tangui Le Pense <29804907+tanguilp@users.noreply.github.com>
2022-01-07 16:33:13 +0300

    Add telemetry events for cleaning processes

--- src/http_cache_store_native_lru_nuker.erl
+++ src/http_cache_store_native_lru_nuker.erl
@@ -24,16 +24,22 @@ handle_cast(_Request, State) ->
     {noreply, State}.
 
 handle_info(check, State) ->
-    nuke(?START_NUKE_NB_OBJECTS),
+    telemetry:span([http_cache_store_native, lru_nuker],
+                   #{},
+                   fun() ->
+                      nuke(?START_NUKE_NB_OBJECTS),
+                      {ok, #{}}
+                   end),
     schedule_check(),
     {noreply, State}.
 
 nuke(NbObjects) ->
     AllocatedMemoryUsed = http_cache_store_native_stats:allocated_memory_used(),
     if AllocatedMemoryUsed < 0.99 ->
-           http_cache_store_native_stats:set_limit_reached(false);
-       ok; AllocatedMemoryUsed < 1 ->
-           case nuke_one_object(NbObjects) of
+           http_cache_store_native_stats:set_limit_reached(false),
+           ok;
+       AllocatedMemoryUsed < 1 ->
+           case nuke_objects(NbObjects) of
                table_empty ->
                    ok;
                ok ->
@@ -41,7 +47,7 @@ nuke(NbObjects) ->
            end;
        true ->
            http_cache_store_native_stats:set_limit_reached(true),
-           case nuke_one_object(NbObjects) of
+           case nuke_objects(NbObjects) of
                table_empty ->
                    ok;
                ok ->
@@ -49,9 +55,9 @@ nuke(NbObjects) ->
            end
     end.
 
-nuke_one_object(0) ->
+nuke_objects(0) ->
     ok;
-nuke_one_object(NbObjects) ->
+nuke_objects(NbObjects) ->
     case ets:first(?LRU_TABLE) of
         '$end_of_table' ->
             table_empty;
@@ -60,13 +66,13 @@ nuke_one_object(NbObjects) ->
                 [{_, _, _, _, _, _, _, SeqNumber}] ->
                     ets:delete(?LRU_TABLE, LRUKey),
                     http_cache_store_native:delete_object(ObjectKey, lru_nuked),
-                    nuke_one_object(NbObjects - 1);
+                    nuke_objects(NbObjects - 1);
                 % either the object exists, but with another sequence number which means the
                 % current LRU key is outdated (a more recent one exists), or the object doesn't
                 % exist anymore
                 _ ->
                     ets:delete(?LRU_TABLE, LRUKey),
-                    nuke_one_object(NbObjects)
+                    nuke_objects(NbObjects)
             end
     end.
 
